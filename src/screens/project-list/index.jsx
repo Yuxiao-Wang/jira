92fab523203f16a27638deaac7ea5a44fac7d128
王昱潇 <aquarius1994@vip.qq.com>
2022-01-22 08:02:21 +0800

    简单实现了表单

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,41 @@
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { useEffect, useState } from "react";
+import React from "react";
+import { cleanObject } from "../utils";
+import qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL
+export const ProjectListScreen =() => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+      if(response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [param])
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if(response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  })
+  return  <div>
+    <SearchPanel users={users} param={param} setParam={setParam}/>
+    <List users={users} list={list}/>
+  </div>
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
