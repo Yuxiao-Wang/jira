df8f287a03cf72da36f46dc8ebf595407f15aadc
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-01-28 15:16:32 +0800

    改造为Typescript编写

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,18 +2,18 @@ import { List } from "./list";
 import { SearchPanel } from "./search-panel";
 import { useEffect, useState } from "react";
 import React from "react";
-import { cleanObject, useDebounce } from "../utils";
-import qs from "qs";
+import {cleanObject, useDebounce, useMount } from "../utils";
+import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL
-export const ProjectListScreen =() => {
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([])
   const [param, setParam] = useState({
     name: '',
     personId: ''
   })
   const [list, setList] = useState([])
-  const debouncedParam = useDebounce(param, 2000)
-  const [users, setUsers] = useState([])
+  const debouncedParam = useDebounce(param,  200)
 
   useEffect(() => {
     fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
@@ -30,13 +30,7 @@ export const ProjectListScreen =() => {
     })
   })
   return  <div>
-    <SearchPanel users={users} param={param} setParam={setParam}/>
+    <SearchPanel users={users}  param={param} setParam={setParam}/>
     <List users={users} list={list}/>
   </div>
-}
-
-export const useMount = (callback) => {
-  useEffect(() => {
-    callback()
-  }, [])
 }
