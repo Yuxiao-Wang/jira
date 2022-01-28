defcdd836b6c25d41518b90a46c832344b718eba
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-01-28 16:20:24 +0800

    chore: any -> unknown

--- src/screens/utils/index.ts
+++ src/screens/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 //在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: object) => {
   const result = { ...object };
@@ -21,7 +21,8 @@ export const useMount = (callback: () => void) => {
   }, [])
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+// 后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
