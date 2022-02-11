e73dfd5a759180d1c6a009c5644047d878daea11
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-02-11 11:40:16 +0800

    feat: 添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { ProjectListScreen } from "./screens/project-list";
-import {TsReactTest} from "./screens/project-list/try-use-array";
-import {LoginScreen} from "./screens/login";
+import {useAuth} from "./context/auth-context";
+import {AuthenticatedApp} from "./authenticated-app";
+import {UnauthenticatedApp} from "./unauthenticated-app";
 
 function App() {
+    const {user} = useAuth()
   return (
     <div className="App">
-      {/*<ProjectListScreen />*/}
-      {/*  <TsReactTest />*/}
-        <LoginScreen />
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp/>}
     </div>
   );
 }
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,21 +1,10 @@
 import {FormEvent} from "react";
-import * as qs from "qs";
-import {cleanObject} from "../utils";
+import {useAuth} from "../context/auth-context";
+
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const LoginScreen = () => {
-    const login = (param: {username: string, password: string}) => {
-        fetch(`${apiUrl}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(param)
-        }).then(async response => {
-            if(response.ok) {
-            }
-        })
-    }
+    const {login, user} = useAuth()
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const username = (event.currentTarget.elements[0] as HTMLInputElement).value
