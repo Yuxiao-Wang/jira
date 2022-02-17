37619d2c897afefc60d54c2e41288ca37230d1e6
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-02-17 11:42:43 +0800

    style:用row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,10 +1,41 @@
 import {ProjectListScreen} from "./screens/project-list";
 import {useAuth} from "./context/auth-context";
+import styled from "@emotion/styled";
+import {Row} from "./components/lib";
 
 export const AuthenticatedApp = () => {
     const {logout} = useAuth()
-    return <div>
-        <button onClick={logout}>登出</button>
-        <ProjectListScreen />
-    </div>
+    return <Container>
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <h2>Logo</h2>
+                <h2>项目</h2>
+                <h2>用户</h2>
+            </HeaderLeft>
+            <HeaderRight>
+                <button onClick={logout}>登出</button>
+            </HeaderRight>
+        </Header>
+        <Main>
+            <ProjectListScreen />
+        </Main>
+    </Container>
 }
+
+const HeaderLeft = styled(Row)`
+  display: flex;
+  align-items: center;
+`
+const HeaderRight = styled.div``
+
+const Header = styled(Row)``
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem calc(100vh - 6rem);
+  height: 100vh;
+`
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`
