2a9a17652db3e1088a937a1651270fee5c8e5f9e
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-03-30 14:08:24 +0800

    feat:用状态提升实现propjectModal状态管理

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,23 +4,31 @@ import { useState } from "react";
 import React from "react";
 import {useDebounce, useDocumentTitle,} from "../../utils";
 import styled from "@emotion/styled";
-import {Button, Typography} from "antd";
+import {Button, Row, Typography} from "antd";
 import {useProjects} from "./project";
 import {useUsers} from "./user";
 import {useUrlQueryParam} from "../../utils/url";
 import {useProjectsSearchParam} from "./util";
 
-export const ProjectListScreen = () => {
+export const ProjectListScreen = (props: {setProjectModalOpen: (isOpen: boolean) => void}) => {
   useDocumentTitle("项目列表", false);
   const [param, setParam] = useProjectsSearchParam()
   const {isLoading, error, data: list, retry} = useProjects(useDebounce(param,  200))
   const {data: users} = useUsers()
 
   return  <Container>
-    <h1>项目列表</h1>
+    <Row justify={'space-between'}>
+      <h1>项目列表</h1>
+      <Button onClick={() => props.setProjectModalOpen(true)}>创建项目</Button>
+    </Row>
     <SearchPanel users={users || []}  param={ param } setParam={setParam}/>
     {error? <Typography.Text type={'danger'}>{error.message}</Typography.Text> : null}
-    <List refresh={retry} loading={isLoading} users={users || []} dataSource={list || []}/>
+    <List
+        setProjectModalOpen={props.setProjectModalOpen}
+        refresh={retry}
+        loading={isLoading}
+        users={users || []}
+        dataSource={list || []}/>
   </Container>
 }
 
--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {User} from "./search-panel";
-import {Table, TableProps} from "antd";
+import {Button, Dropdown, Menu, Table, TableProps} from "antd";
 import dayjs from "dayjs";
 import {Link} from "react-router-dom";
 import {Pin} from "../../components/pin";
 import {useEditProject} from "./project";
+import {ButtonNoPadding} from "../../components/lib";
 
 // TODO 把所有ID都改成number类型
 export interface Project {
@@ -18,6 +19,7 @@ export interface Project {
 interface ListProps extends TableProps<Project>{
   users: User[],
   refresh?: () => void,
+  setProjectModalOpen:(isOpen: boolean) => void
 }
 
 export const List = ({users, ...props}: ListProps) => {
@@ -60,6 +62,17 @@ export const List = ({users, ...props}: ListProps) => {
                        {project.created ? dayjs(project.created).format('YYYY-MM-DD') : '无'}
                      </span>
                      }
+                 },
+                 {
+                       render(value, project) {
+                           return <Dropdown overlay={<Menu>
+                               <Menu.Item key={'edit'}>
+                                   <ButtonNoPadding type={"link"} onClick={() => props.setProjectModalOpen(true)}>编辑</ButtonNoPadding>
+                               </Menu.Item>
+                           </Menu>}>
+                               <ButtonNoPadding type={"link"}>...</ButtonNoPadding>
+                           </Dropdown>
+                       }
                  }
                ]}
       {...props}
