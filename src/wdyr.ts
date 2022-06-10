b8acb4c30e95d58b8435422aa18472b23603c513
yuxiao-wang <yuxiao.wang@deepfinance.com>
2022-06-10 17:47:33 +0800

    Updates

--- src/wdyr.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-if(process.env.NODE_ENV === 'development') {
-    const whyDidYouRender = require('@welldone-software/why-did-you-render')
-    whyDidYouRender(React, {
-        trackAllPureComponents: false // 它会跟踪所有的函数组件，所以设置为false
-    })
-}
